da355318ce1fc0b04c7ecca44e58d029653e480c
devhong <popodaddy@gmail.com>
2026-08-14 11:30:50 +0900

    PromptTemplate 사용

--- src/main.py
+++ src/main.py
@@ -9,7 +9,16 @@ from langchain_core.prompts import PromptTemplate
 llm = ChatOllama(model="llama3:8b", base_url="http://172.16.0.130:11434", temperature=1)
 
 # llm_answer = llm.invoke("오늘은 기분 나쁜 그놈을 또 보고 말았네, 나의 기분을 1줄로 응원해줘. 대답은 반드시 한국어로 해줘")
-llm_answer = llm.invoke("기분 좋은 시를 하나 만들어줘. 대답은 한국어로 해")
+# llm_answer = llm.invoke("기분 좋은 시를 하나 만들어줘. 대답은 한국어로 해")
+
+prompt = PromptTemplate(
+    template="너는 한국사람이고 모든 대답은 한국어로만 할 수 있어, {query}",
+    input_variables=["query"]
+)
+
+chain = prompt | llm
+
+llm_answer = chain.invoke({"query": "blue는 칼라코드가 어떻게되지?"})
 
 print("--- reply ---")
 print(llm_answer.content)
